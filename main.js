26017524b45f1556cf94bfc35c6202cd62556bf3
ngndangg <107494953+ngndangg@users.noreply.github.com>
2022-06-17 19:49:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 import { getFirestore, doc, getDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
