121d5e05256f6aac15ec8350b64fd699cfcf5803
ngndang <ngnhaidangvn@gmail.com>
2022-09-15 01:58:11 +0700

    Bug fix

--- JS/cookies.js
+++ JS/cookies.js
@@ -5,6 +5,10 @@ export var thisId = 0;
 
 const deviceIds = doc(db, "Batluc/deviceIds");
 
+/**
+ * @description Get Ids from database and split into array
+ * @return {Promise} Needs "await" to return the value
+ */
 async function GetDeviceIdsArray() {
     const snapshot = await getDoc(deviceIds);
     if (snapshot.exists()) {
@@ -15,6 +19,10 @@ async function GetDeviceIdsArray() {
     }
 }
 
+/**
+ * @description Get Ids from database as a single string
+ * @return {Promise} Needs "await" to return the value
+ */
 async function GetDeviceIdsString() {
     const snapshot = await getDoc(deviceIds);
     if (snapshot.exists()) {
@@ -26,9 +34,14 @@ async function GetDeviceIdsString() {
     }
 }
 
+/**
+ * @description Write an device id to DB by appending the id to a string
+ * @param {number} _id id to write to DB 
+ */
 function WriteDeviceIds(_id) {
     var oldIds = savedString;
     var str = ""
+        //Add id to string
     if (oldIds == "") {
         str = _id;
     } else {
@@ -48,21 +61,26 @@ function WriteDeviceIds(_id) {
         })
 }
 
-
+/**
+ * @description Get cookie from the browser and break it up into a dictionary
+ * @param {string} _name name of the cookie to look for
+ */
 function GetCookie(_name) {
     var dict = document.cookie.split(';')
         .map(cookie => cookie.split('='))
         .reduce((accumulator, [key, value]) => ({...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
-    return dict[_name];
+    return dict[_name]; // >> value of "_name:..."
 }
 
 function DeleteId() {
-    document.cookie = "Id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    document.cookie = "Id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/"; //Put this in console to clear that device's id
 }
 
-var gotcookie = GetCookie("Id")
-if (gotcookie == undefined) {
-    var newid = Math.floor(Math.random() * 100000);
+//=================================== MAIN ====================================
+
+var gotcookie = GetCookie("Id") //Check browser for Id cookie
+if (gotcookie == undefined) { //Generate a new one if didn't exist
+    var newid = Math.floor(Math.random() * 100000); //Worst Id generator ever
     document.cookie = "Id=" + newid.toString() + "; expires=Thu, 31 Dec 2030 12:00:00 UTC; path=/";
     gotcookie = newid;
 } else {
@@ -73,6 +91,7 @@ thisId = gotcookie;
 var savedIds = await GetDeviceIdsArray();
 var savedString = await GetDeviceIdsString();
 var saved = false
+    //Search the database's ids string(splitted into array) for this device's id
 savedIds.forEach(_id => {
     if (_id == thisId) {
         console.log("This device is already saved");
@@ -80,6 +99,7 @@ savedIds.forEach(_id => {
         return;
     }
 });
+//If not saved write this id to database
 if (!saved) {
     WriteDeviceIds(thisId);
 }
